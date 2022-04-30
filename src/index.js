4c0ecdc5b189adb6aef066dbf6e12e99eadccbac
Lada Šilhavá <lada.silhava@email.cz>
2022-04-30 12:01:55 +0200

    order btn and filling cup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 console.log('funguju!');
-
+//Navigace (3)
 document.querySelector('#nav-btn').addEventListener('click', () => {
     document.querySelector('nav').classList.toggle('nav-closed')
 })
@@ -12,3 +12,20 @@ navElm.forEach((item) => {
     document.querySelector('nav').classList.toggle('nav-closed')
 })}
 )
+
+//Objednání (4)
+const orderBtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+let ordered = false;
+
+orderBtn.addEventListener('click', () => {
+    if (ordered) {
+    orderBtn.textContent = 'Objednat',
+    drinkCup.classList.remove('drink__cup--selected');
+    ordered = false;
+    } else {
+        orderBtn.textContent = 'Zrušit';
+        drinkCup.classList.add('drink__cup--selected');
+        ordered = true;
+    }
+});
