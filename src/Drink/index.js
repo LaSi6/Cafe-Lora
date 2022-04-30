431202809ab34463133130f6dd7a1912bd99e392
Lada Šilhavá <lada.silhava@email.cz>
2022-04-30 15:37:21 +0200

    dynamic component for drink - ordered button

--- src/Drink/index.js
+++ src/Drink/index.js
@@ -2,7 +2,8 @@ import { Layer } from '../Layer/index.js';
 import './style.css';
 
 export const Drink = (props) => {
-    const {id, name, ordered, layers, image} = props;
+    const {id, name, layers, image} = props;
+    let {ordered} = props;
     const drinkElm = document.createElement('div');
     drinkElm.classList.add('drink')
     drinkElm.innerHTML += `
@@ -21,6 +22,21 @@ export const Drink = (props) => {
     const drinkIngr = drinkElm.querySelector('.drink__info');
     layers.forEach((item) => {
         drinkIngr.appendChild(Layer(item));
+    });
+
+    const orderBtn = drinkElm.querySelector('.order-btn');
+    const drinkCup = drinkElm.querySelector('.drink__cup');
+
+    orderBtn.addEventListener('click', () => {
+    if (ordered) {
+    orderBtn.textContent = 'Objednat',
+    drinkCup.classList.remove('drink__cup--selected');
+    ordered = false;
+    } else {
+        orderBtn.textContent = 'Zrušit';
+        drinkCup.classList.add('drink__cup--selected');
+        ordered = true;
+    }
     });
 
     return drinkElm  
