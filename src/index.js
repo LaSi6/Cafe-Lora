e88b2d7df13d701eb016bdfa4fd68e650d1822c0
Lada Šilhavá <lada.silhava@email.cz>
2022-04-30 11:29:18 +0200

    js hide/show nav

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
 
 console.log('funguju!');
+
+document.querySelector('#nav-btn').addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('nav-closed')
+})
+
+const navElm = document.querySelectorAll('nav a')
+navElm.forEach((item) => {
+    item.addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('nav-closed')
+})}
+)
